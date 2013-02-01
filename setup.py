bac13d12afbaab15f41ccb3ac27d5ab0451be888
Matthew Brett <matthew.brett@gmail.com>
2013-01-31 19:08:03 -0800

    RF: switch to console entry point

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from os.path import join as pjoin
+import setuptools
 from distutils.core import setup
 
 setup(
     name='myscripter',
     version='1.0',
-    scripts=[pjoin('bin', 'myscript')]
+    packages = ['myscripter'],
+    entry_points = {
+        'console_scripts': [
+            'my_console_script = myscripter.commands:my_console_script']
+    }
     )
